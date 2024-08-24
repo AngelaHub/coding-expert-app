d40710bdd707dccfdd08aaee8af010f58cb687ed
AngelaHub <anjela-2005@hotmail.com>
2024-08-24 17:02:23 +0200

    CategoryView clean up

--- screens/CategoryView.js
+++ screens/CategoryView.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook
+import { useNavigation } from '@react-navigation/native'; 
 
 const CategoryView = () => {
   const navigation = useNavigation();
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
 
   const handleCategoryPress = (id) => {
-    setSelectedCategoryId(id); // Store the selected category ID
+    setSelectedCategoryId(id); 
     console.log(`Selected CATEGORY ID: ${id}`)
-    navigation.navigate('GenreView', { categoryId: id }); // Pass the ID to the next screen
+    navigation.navigate('GenreView', { categoryId: id }); 
   };
 
   return (
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
-    paddingHorizontal: 60, // Add padding for better layout on small screens
+    paddingHorizontal: 60, 
   },
   title: {
     fontSize: 20,
@@ -60,12 +60,12 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.25,
     shadowRadius: 15,
-    elevation: 5,  // Needed for Android shadow
-    marginBottom: 44, // Margin between buttons
+    elevation: 5,  
+    marginBottom: 44, 
   },
   buttonText: {
     color: '#fff',
-    fontSize: 16, // Reduced size for better fit
+    fontSize: 16, 
     fontFamily: 'Poppins-SemiBold',
   },
 });
